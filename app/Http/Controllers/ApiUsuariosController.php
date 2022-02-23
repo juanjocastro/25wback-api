5abc327507d0a43bfaf57bf73cfbd8e305a0680b
juanjocastro <juanjocastro@gmail.com>
2022-02-23 19:38:36 -0300

    prueba 00001

--- app/Http/Controllers/ApiUsuariosController.php
+++ app/Http/Controllers/ApiUsuariosController.php
@@ -20,7 +20,7 @@ class ApiUsuariosController extends Controller
                 /* validar los datos que llegan, en caso de no ser correcto, hay que mostrar el error en pantalla (react, mediante el status). se me ocurre que mediante el return de laravel, le puedo enviar una respuesta a react y de alli sumarlo a la logica empleada en el componenete coontact y fromMensaje  */
 
 
-		dd('saveusuarios');
+		return 'ESTOY EN SAVEUSUARIOS';
 
                 Usuarios::create([
                     'name' => $request->name,
